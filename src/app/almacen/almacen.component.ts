dd4cd800a89c11c4393f3d583373ecbcd83f4179
Ignacio S <isvekia@tuta.io>
2024-12-16 13:44:21 +0100

    agregadas rutas

---
+++ src/app/almacen/almacen.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-almacen',
+  standalone: true,
+  imports: [RouterOutlet],
+  templateUrl: './almacen.component.html',
+  styles: ``
+})
+export class AlmacenComponent {
+
+}
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,25 @@
 import { Routes } from '@angular/router';
+import { AlmacenComponent } from './almacen/almacen.component';
+import { LoginComponent } from './inicio/login/login.component';
+import { NotFoundComponent } from './inicio/not-found/not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: '/login', pathMatch: 'full' },
+    { path: 'login', component: LoginComponent },
+    {
+      path: 'almacen',
+      component: AlmacenComponent,
+      children: [
+        { path: '', redirectTo: '/almacen/categorias', pathMatch: 'full' },
+        {
+          path: 'categorias',
+          loadComponent: () => import('./almacen/categorias/categorias.component').then((c) => c.CategoriasComponent)
+        },
+        {
+          path: 'productos',
+          loadComponent: () => import('./almacen/productos/productos.component').then((c) => c.ProductosComponent)
+        }
+      ]
+    },
+    { path: '**', component: NotFoundComponent }
+  ];
